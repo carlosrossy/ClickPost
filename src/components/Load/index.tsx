8f965cd115a5793b37f2dbb32af2507b50be83d5
CarlosBitizin <carlospintorossy07@gmail.com>
2022-05-01 01:40:06 -0400

    Feat: Load Component

---
+++ src/components/Load/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ActivityIndicator } from 'react-native';
+
+export function Load(){
+
+    return(
+
+        <ActivityIndicator
+                size={'large'}
+                color = '#0E1647'
+            />
+    )
+}
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,6 +2,7 @@ import React, {useEffect,useState} from 'react';
 import { ActivityIndicator, FlatList, Text} from 'react-native';
 
 import HeaderPages from '../../components/HeaderPages';
+import { Load } from '../../components/Load';
 import api from '../../services/api';
 
 import { Container } from './style';
@@ -34,10 +35,7 @@ export function Home (){
 
         <Container>
             <HeaderPages title='Post'/>
-            {load ? <ActivityIndicator
-                size={'large'}
-                color = '#000000'
-            />:
+            {load ? <Load/>:
                 <FlatList
                     data = {post}
                     keyExtractor = {(item) => String(item.id)}
